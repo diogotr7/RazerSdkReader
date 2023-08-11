f47df6a9c77ff61c56be3dcc2422f30249dbde88
Diogo Trindade <diogotr7@gmail.com>
2023-08-11 17:24:20 +0100

    move file

--- src/RazerEmulatorReader.Benchmarks/MemoryMappedFileProxy4.cs
+++ src/RazerEmulatorReader.Benchmarks/MemoryMappedFileProxy4.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 namespace RazerEmulatorReader.Benchmarks;
 
-public unsafe class MemoryMappedFileProxy3 : IDisposable
+public unsafe class MemoryMappedFileProxy4 : IDisposable
 {
     private readonly MemoryMappedFile _file;
     private readonly MemoryMappedViewAccessor _view;
@@ -12,7 +12,7 @@ public unsafe class MemoryMappedFileProxy3 : IDisposable
     internal string Name { get; }
     internal int Size { get; }
 
-    internal MemoryMappedFileProxy3(string name, int size)
+    internal MemoryMappedFileProxy4(string name, int size)
     {
         Name = name;
         Size = size;
@@ -26,15 +26,15 @@ public unsafe class MemoryMappedFileProxy3 : IDisposable
             _file = MemoryMappedFile.CreateNew(Name, Size, MemoryMappedFileAccess.Read, MemoryMappedFileOptions.None, HandleInheritability.None);
         }
         _view = _file.CreateViewAccessor(0, Size, MemoryMappedFileAccess.Read);
-
+        
         byte* ptr = null;
         _view.SafeMemoryMappedViewHandle.AcquirePointer(ref ptr);
         _pointer = new IntPtr(ptr);
     }
 
-    internal T Read<T>() where T : struct
+    internal T Read<T>() where T : unmanaged
     {
-        return Marshal.PtrToStructure<T>(_pointer);
+        return MemoryMarshal.AsRef<T>(new ReadOnlySpan<byte>(_pointer.ToPointer(), Size));
     }
 
     protected virtual void Dispose(bool disposing)
